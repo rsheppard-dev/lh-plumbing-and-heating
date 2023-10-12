760e6834a1b0b0f22215df4c79fccfdc90693d87
Roy Sheppard <rsheppard83@gmail.com>
2023-10-12 19:40:00 +0100

    layout shift

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/Footer';
-import '../globals.css';
+import './globals.css';
 import Header from '@/components/Header';
 import ISettings from '@/interfaces/ISettings';
 import { settingsQuery } from '@/sanity/lib/queries';
