54676d3b2c21c90731c2a8de582f112277c6b585
Roy Sheppard <rsheppard83@gmail.com>
2023-11-06 23:04:47 +0000

    downgraded next again

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, Suspense } from 'react';
+import '../globals.css';
+
+type Props = {
+	children: ReactNode;
+};
+
+export default async function RootLayout({ children }: Props) {
+	return (
+		<html lang='en'>
+			<body>
+				<Suspense>{children}</Suspense>
+			</body>
+		</html>
+	);
+}
